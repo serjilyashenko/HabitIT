05afcbafdb765b51e359467fb4c6ee1b7f432e92
Serge Ilyashenko <age_bookend_0j@icloud.com>
2026-01-09 15:32:05 +0100

    fix(tests): fix fakeTimer tests

--- src/components/App.test.jsx
+++ src/components/App.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen, waitFor, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { App } from './App';
 import * as mainScreenModule from './MainScreen';
@@ -13,8 +13,16 @@ beforeEach(() => {
   vi.restoreAllMocks(); // for spyOn-s
 });
 
+function setup() {
+  const user = userEvent.setup();
+  return {
+    user,
+    ...render(<App />),
+  };
+}
+
 test('It is possible to complete and un-complete a habit', async () => {
-  render(<App />);
+  const { user } = setup();
 
   const firstHabit = screen.getByLabelText(/first(.*)habit/i);
   const secondHabit = screen.getByLabelText(/second(.*)habit/i);
@@ -22,43 +30,43 @@ test('It is possible to complete and un-complete a habit', async () => {
   expect(firstHabit).toBeChecked();
   expect(secondHabit).not.toBeChecked();
 
-  await userEvent.click(firstHabit);
-  await userEvent.click(secondHabit);
+  await user.click(firstHabit);
+  await user.click(secondHabit);
   expect(firstHabit).not.toBeChecked();
   expect(secondHabit).toBeChecked();
 
-  await userEvent.click(firstHabit);
-  await userEvent.click(secondHabit);
+  await user.click(firstHabit);
+  await user.click(secondHabit);
   expect(firstHabit).toBeChecked();
   expect(secondHabit).not.toBeChecked();
 });
 
-test('All habits are off on the next day', () => {
-  vi.useFakeTimers().setSystemTime(new Date('2022-04-10'));
+test('All habits are off on the next day', async () => {
+  vi.setSystemTime(new Date('2022-04-10'));
 
-  const { rerender } = render(<App />);
-  userEvent.click(screen.getByLabelText(/first(.*)test(.*)habit/i));
-  userEvent.click(screen.getByLabelText(/second(.*)habit/i));
+  const { user } = setup();
+  await user.click(screen.getByLabelText(/first(.*)test(.*)habit/i));
+  await user.click(screen.getByLabelText(/second(.*)habit/i));
 
-  vi.useFakeTimers().setSystemTime(new Date('2022-04-11'));
-  rerender(<App />);
+  vi.setSystemTime(new Date('2022-04-11'));
+  await fireEvent.focus(window);
 
   expect(screen.getByLabelText(/first(.*)habit/i)).not.toBeChecked();
   expect(screen.getByLabelText(/second(.*)habit/i)).not.toBeChecked();
 });
 
 test('Don`t crushes if complete on the next day', async () => {
-  vi.useFakeTimers().setSystemTime(new Date('2022-04-10'));
-  render(<App />);
+  vi.setSystemTime(new Date('2022-04-10'));
 
-  vi.useFakeTimers().setSystemTime(new Date('2022-04-11'));
-  fireEvent.focus(window); // click browser tab
+  const { user } = setup();
 
-  expect(screen.getByLabelText(/first(.*)habit/i)).not.toBeChecked();
+  expect(screen.getByLabelText(/first(.*)habit/i)).toBeChecked();
+  vi.setSystemTime(new Date('2022-04-11'));
+  await fireEvent.focus(window);
 
-  userEvent.click(screen.getByLabelText(/first(.*)habit/i));
+  await user.click(screen.getByLabelText(/first(.*)habit/i));
 
-  await waitFor(expect(screen.getByLabelText(/first(.*)habit/i)).toBeChecked);
+  expect(screen.getByLabelText(/first(.*)habit/i)).toBeChecked();
 });
 
 test('Shows error message if a component crush the app', async () => {
--- src/components/HabitWorkSpace.test.jsx
+++ src/components/HabitWorkSpace.test.jsx
@@ -15,18 +15,22 @@ afterEach(() => {
   vi.restoreAllMocks(); // for spyOn-s
 });
 
-function renderWithProviders() {
-  render(<HabitWorkSpace />, {
-    wrapper: ({ children }) => (
-      <TodayProvider>
-        <HabitProvider>{children}</HabitProvider>
-      </TodayProvider>
-    ),
-  });
+function setup() {
+  const user = userEvent.setup();
+  return {
+    user,
+    ...render(<HabitWorkSpace />, {
+      wrapper: ({ children }) => (
+        <TodayProvider>
+          <HabitProvider>{children}</HabitProvider>
+        </TodayProvider>
+      ),
+    }),
+  };
 }
 
 test('It shows habit checklist by default', () => {
-  renderWithProviders();
+  setup();
 
   expect(
     screen.queryByRole('button', { name: /done/i })
@@ -45,10 +49,10 @@ test('It shows habit checklist by default', () => {
 });
 
 test('Edit mode delete success', async () => {
-  renderWithProviders();
+  const { user } = setup();
 
   const editButton = screen.getByRole('button', { name: /edit/i });
-  await userEvent.click(editButton);
+  await user.click(editButton);
 
   const doneButton = screen.getByRole('button', { name: /done/i });
   expect(editButton).not.toBeInTheDocument();
@@ -57,9 +61,9 @@ test('Edit mode delete success', async () => {
   const deleteSecondHabitButton = within(
     screen.getByText(/second(.*)habit/i).parentNode
   ).getByLabelText('delete');
-  await userEvent.click(deleteSecondHabitButton);
+  await user.click(deleteSecondHabitButton);
 
-  await userEvent.click(doneButton);
+  await user.click(doneButton);
   expect(screen.getByLabelText(/first(.*)habit/i)).toBeInTheDocument();
   expect(screen.getByLabelText(/first(.*)habit/i)).toHaveAttribute(
     'type',
@@ -70,17 +74,17 @@ test('Edit mode delete success', async () => {
 });
 
 test('Analytics mode switch', async () => {
-  renderWithProviders();
+  const { user } = setup();
 
   const analyticsButton = screen.getByRole('button', { name: /analytics/i });
-  await userEvent.click(analyticsButton);
+  await user.click(analyticsButton);
 
   const doneButton = screen.getByRole('button', { name: /done/i });
   expect(analyticsButton).not.toBeInTheDocument();
   expect(doneButton).toBeInTheDocument();
   expect(screen.queryAllByRole('table')).toHaveLength(2 * 2);
 
-  await userEvent.click(doneButton);
+  await user.click(doneButton);
   expect(screen.getByLabelText(/first(.*)habit/i)).toBeInTheDocument();
   expect(screen.queryByLabelText(/second(.*)habit/i)).toBeInTheDocument();
 });
